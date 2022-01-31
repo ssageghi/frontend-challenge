1c985d05bfb2edd9fe214a8d652564793c7107e1
ssageghi <ssagegghi@gmail.com>
2022-01-31 18:12:30 +0330

    Develope component completed

--- src/components/Developer/Developer.tsx
+++ src/components/Developer/Developer.tsx
@@ -1,9 +1,41 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react';
 import { DeveloperItem } from '../../interfaces/DeveloeprInterfaces';
-const Developer: FunctionComponent<{ repository: DeveloperItem }> = (props) => {
-  const repo = props;
-  return <div className='row'>
+import { repoIcon, popularIcon, heartIcon } from '../../icons';
+import { Button } from 'antd';
+import "./Developer.scss";
+const Developer: FunctionComponent<{ developer: DeveloperItem, index: number }> = (props) => {
+  const { developer, index } = props;
+  const profileUrl = `https://github.com/${developer.username ? developer.username : developer.author}`;
+  return <div className='row dev-container'>
+    <div className="dev-container__avatar">
+      <p>{index + 1}</p>
+      <img src={developer.avatar} alt="" />
+      <div className='dev-container__user-info'>
+        <a href={profileUrl}>
+          <h1>
+            {developer.author}</h1>
+        </a>
 
+        {developer.username ? <a href={profileUrl}><h3>{developer.username}</h3></a> : ""}
+      </div>
+    </div>
+    <div className="dev-container__repo-information">
+      <div>
+        {popularIcon}
+        PUPULAR REPO
+      </div>
+      <a href={developer.repourl}>
+        {repoIcon}
+        {developer.reponame}
+      </a>
+      <div>
+        {developer.description}
+      </div>
+    </div>
+    <div className="dev-container__action-buttons">
+      {developer.sponsorUrl ? <Button className='support-btn'>{heartIcon}Sponsor</Button> : ""}
+      <Buttondev-container__repo-information>Follow</Buttondev-container__repo-information>
+    </div>
   </div>;
 }
 
--- src/components/DevelopersContainer/DevelopersContainer.tsx
+++ src/components/DevelopersContainer/DevelopersContainer.tsx
@@ -1,10 +1,18 @@
 import React, { FunctionComponent } from 'react';
 import { DevelopersProps } from '../../interfaces/DeveloeprInterfaces'
-
+import Developer from '../Developer';
 
 const DevelopersContainer: FunctionComponent<DevelopersProps> = (props) => {
-  console.log(props);
-  return <div></div>;
+  const { developers } = props;
+
+  return (<>
+    {
+      developers?.map((developer, index) => (
+        <Developer developer={developer} key={developer.author} index={index} />
+      ))
+
+    }
+  </>);
 }
 
 export default DevelopersContainer;
