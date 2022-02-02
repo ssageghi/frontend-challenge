226b17184ddbc9e0bf102d52496532164a48a6f2
ssageghi <ssagegghi@gmail.com>
2022-02-02 16:40:57 +0330

    unnessary code removed

--- src/components/Developer/Developer.tsx
+++ src/components/Developer/Developer.tsx
@@ -5,18 +5,17 @@ import { Button } from 'antd';
 import "./Developer.scss";
 const Developer: FunctionComponent<{ developer: DeveloperItem, index: number }> = (props) => {
   const { developer, index } = props;
-  const profileUrl = `https://github.com/${developer.username ? developer.username : developer.author}`;
   return <div className='row dev-container'>
     <div className="dev-container__avatar">
       <p>{index + 1}</p>
       <img src={developer.avatar} alt="" />
       <div className='dev-container__user-info'>
-        <a href={profileUrl}>
+        <a href={developer.url}>
           <h1>
             {developer.author}</h1>
         </a>
 
-        {developer.username ? <a href={profileUrl}><h3>{developer.username}</h3></a> : ""}
+        {developer.username ? <a href={developer.url}><h3>{developer.username}</h3></a> : ""}
       </div>
     </div>
     <div className="dev-container__repo-information">
